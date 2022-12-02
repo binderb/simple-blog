8c12859167674a9f4bcbf1725bdec0d62595f395
Ben Binder <ben.binder11@gmail.com>
2022-12-02 14:16:50 -0600

    Add sequelize database connection.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,7 @@ app.use(express.json);
 app.use(express.urlencoded({extended: true}));
 
 async function init () {
+  await sequelize.sync({alter: true});
   app.listen(PORT, () => console.log(`Listening on ${PORT}.`));
 }
 
